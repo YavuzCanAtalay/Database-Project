726894b66013c8da63cbfc24d3f68d1a6736be2a
Yavuz Can Atalay <yavuz.atalay@sabanciuniv.edu>
2025-04-01 19:10:42 +0300

    Update insert_review.php

--- CS306_GROUP_72_HW2/build/insert_review.php
+++ CS306_GROUP_72_HW2/build/insert_review.php
@@ -2,7 +2,7 @@
 
 $servername = "localhost";       
 $username = "root";              
-$password = "*****";         
+$password = "*****"; //I deleted         
 $dbname = "cs306";  
 
 $conn = new mysqli($servername, $username, $password, $dbname);
